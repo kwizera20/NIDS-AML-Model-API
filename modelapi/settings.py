2e543045a25a228cf80e8406e2e91f5450c4e65b
= <=>
2024-09-26 13:20:19 +0200

    Added the static files for deployment purpose

--- modelapi/settings.py
+++ modelapi/settings.py
@@ -17,6 +17,9 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Define the STATIC_URL and STATIC_ROOT
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Example path
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
